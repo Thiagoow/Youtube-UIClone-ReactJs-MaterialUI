2c50a3fba485d079cf25336a13be85c1dc82f4be
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2022-03-23 19:25:19 -0300

    Using global theme with default customization 😁

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,7 @@
 import React from "react";
-import { ThemeProvider, createTheme } from "@mui/material";
-import { red, blue } from "@mui/material/colors";
+import { ThemeProvider } from "@mui/material";
+import theme from "./theme";
 import Home from "./pages/Home";
-
-/*Export this const won't change nothing❗
-
-To make export a custom theme, use "theme.jsx"
-file inside src folder has base, change the
-values, and just use it in other file adding:
-import theme from '../theme'; */
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: red[500]
-    },
-    secondary: {
-      main: blue[500]
-    }
-  },
-  typography: {
-    button: {
-      textTransform: "none"
-    }
-  }
-});
 
 function App() {
   return (
