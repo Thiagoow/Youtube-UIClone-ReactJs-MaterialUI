4509300bbf342ce297d975c26e3e168171372626
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2022-03-25 19:40:41 -0300

    Creating the videoListing component :D

---
+++ src/components/VideoListing.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import theme from "../theme";
+import { styled } from "@mui/material";
+//Components:
+import { AppBar, Toolbar, IconButton, Button } from "@mui/material";
+//Icons:
+import { Menu, AccountCircle, Mic, Apps, MoreVert } from "@mui/icons-material";
+//Images:
+import darkLogoImg from "../../assets/img/darkLogo.png";
+
+export default function VideoListing() {
+  return <div>VideoListing</div>;
+}
